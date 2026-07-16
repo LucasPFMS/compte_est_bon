38708d64c325052cf87fc262f2251a26c6aab3a0
Lucas Pillon <lucas.pillon@fms-ea.com>
2026-07-16 09:01:47 +0200

    modification draw

--- compte_est_bon.py
+++ compte_est_bon.py
@@ -6,12 +6,8 @@ import random
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def draw(numbers):
-    choice: list = []
-    n = 0
-    while n < len(numbers):
-        choice.append(random.choice(numbers))
-        n += 1
+def draw(choice, number):
+    choice.append(number)
     return choice
     # Use a breakpoint in the code line below to debug your script.
     #  Press Ctrl+F8 to toggle the breakpoint.
@@ -20,6 +16,13 @@ def draw(numbers):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     all_numbers: list = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 25, 50, 75, 100]
+    m = 0
+    my_numbers: list = []
+    while m < len(all_numbers):
+        random_number = random.choice(all_numbers)
+        draw(my_numbers, random_number)
+
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
