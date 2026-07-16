954ce8c2b36b1c278ada3cf9012b189f1f46dc38
Lucas Pillon <lucas.pillon@fms-ea.com>
2026-07-16 09:13:22 +0200

    ajout de final_number

--- compte_est_bon.py
+++ compte_est_bon.py
@@ -13,9 +13,14 @@ def draw(choice, number):
     #  Press Ctrl+F8 to toggle the breakpoint.
 
 
-def del_number(numbers,number):
+def del_number(numbers, number):
     numbers.remove(number)
     return numbers
+
+
+def final_number():
+    number = random.randint(101, 999)
+    return number
 
 
 # Press the green button in the gutter to run the script.
@@ -23,11 +28,12 @@ if __name__ == '__main__':
     all_numbers: list = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 25, 50, 75, 100]
     m = 0
     my_numbers: list = []
-    while m < len(all_numbers):
+    print("voici tes nombres: ")
+    while m < 6:
         random_number = random.choice(all_numbers)
         draw(my_numbers, random_number)
-
-
-
-
+        del_number(all_numbers, random_number)
+        print(my_numbers[m])
+        m += 1
+    final = final_number()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
